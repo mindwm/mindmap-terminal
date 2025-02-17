75ded5d8524722ed1099650134cd64af02944dad
Sergey Ryabin <ryabin.ss@gmail.com>
2025-02-17 19:28:37 +0100

    chore(terminal): switch to xterm

--- bin/mindwm-terminal.sh
+++ bin/mindwm-terminal.sh
@@ -4,7 +4,8 @@ TMUX_SESSION_NAME="${SESSION_ID}"
 #TMUX_SESSION_NAME="mindwm-terminal-`date +%s`"
 WINDOW_TITLE="${TMUX_SESSION_NAME}"
 
-gnome-terminal -t "${WINDOW_TITLE}" -- sh -c "tmux new -s ${TMUX_SESSION_NAME}"
+#gnome-terminal -t "${WINDOW_TITLE}" -- sh -c "tmux new -s ${TMUX_SESSION_NAME}"
+xterm -fa "Monospace" -fs 14 -e sh -c "tmux new -s ${TMUX_SESSION_NAME}" &
 while :; do
     sleep 1
     tmux ls | grep "${TMUX_SESSION_NAME}" && break
--- bin/send-keys.sh
+++ bin/send-keys.sh
@@ -3,4 +3,6 @@ TMUX_SESSION_NAME="$1"
 shift
 TMUX_PANE_ID="0"
 
-tmux send-keys -t "${TMUX_SESSION_NAME}:${TMUX_PANE_ID}" "$*" c-m
+tmux send-keys -t "${TMUX_SESSION_NAME}:${TMUX_PANE_ID}" "$*" 
+sleep 0.2
+tmux send-keys -t "${TMUX_SESSION_NAME}:${TMUX_PANE_ID}" "$(printf "\r")"
