f5be2569170873d680f20c08ce7fbb3e381f9ebe
Sergey Ryabin <ryabin.ss@gmail.com>
2025-02-21 19:18:45 +0100

    feat: add org.mindwm.manager groovy package
    send cloudevent using HTTP instead of exec tmux send-keys

---
+++ 1.12.x/lib/org/mindwm/manager/Manager.groovy
@@ -0,0 +1,72 @@
+@Grab('io.cloudevents:cloudevents-core:2.4.0')
+@Grab('com.fasterxml.jackson.core:jackson-databind:2.15.0')
+@Grab('com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.15.0')
+
+import io.cloudevents.CloudEvent
+import io.cloudevents.core.v1.CloudEventBuilder
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+
+import io.cloudevents.CloudEventData
+import io.cloudevents.core.data.PojoCloudEventData
+import io.cloudevents.core.data.BytesCloudEventData
+
+import java.time.OffsetDateTime
+
+import java.net.URI
+import java.time.OffsetDateTime
+import java.util.UUID
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import com.fasterxml.jackson.databind.SerializationFeature
+import java.nio.charset.StandardCharsets
+
+
+
+//println "MINDWM_MANAGER_HTTP_ENDPOINT: ${MINDWM_MANAGER_HTTP_ENDPOINT}"
+
+class MindwmManager {
+
+  String endpoint = System.getenv('MINDWM_MANAGER_HTTP_ENDPOINT') ?: 'http://localhost:38080'
+
+  CloudEvent sendCloudEventHTTP(CloudEvent event) {  
+    def post = new URL(endpoint + "/post").openConnection();
+    def mapper = new ObjectMapper()
+    mapper.disable(SerializationFeature.FAIL_ON_EMPTY_BEANS)
+    mapper.registerModule(new JavaTimeModule())
+    def message = mapper.writeValueAsString(event)
+    post.setRequestMethod("POST")
+    post.setDoOutput(true)
+    post.setRequestProperty("Content-Type", "application/json")
+    post.getOutputStream().write(message.getBytes("UTF-8"));
+    def postRC = post.getResponseCode();
+    println(postRC);
+    if (postRC.equals(200)) {
+        println(post.getInputStream().getText());
+    }
+  } 
+  CloudEvent sendCloudEvent(String source, String type, String subject, String dataPayload) {  
+
+    def objectMapper = new ObjectMapper()
+
+    def map = objectMapper.readValue(dataPayload, Map)
+
+    CloudEventData cloudEventData = PojoCloudEventData.wrap(map) { obj ->
+      objectMapper.writeValueAsBytes(obj)
+    }
+
+    /*
+    println cloudEventData
+    */
+  
+    def cloudEvent = new CloudEventBuilder()
+        .withId(UUID.randomUUID().toString())
+        .withDataContentType('application/json')
+        .withSource(URI.create(source))
+        .withType(type)
+        .withTime(OffsetDateTime.now())
+        .withSubject(subject)
+        .withData("application/json", cloudEventData)
+        .build()
+    return sendCloudEventHTTP(cloudEvent)
+  }
+} 
--- 1.12.x/lib/terminal_listener.groovy
+++ 1.12.x/lib/terminal_listener.groovy
@@ -1,7 +1,31 @@
+@Grab('io.cloudevents:cloudevents-core:2.4.0')
+@Grab('com.fasterxml.jackson.core:jackson-databind:2.15.0')
+@Grab('com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.15.0')
+
 import org.freeplane.api.Node
 import org.freeplane.api.NodeChangeListener
 import org.freeplane.api.NodeChanged
 import org.freeplane.core.util.TextUtils
+import org.mindwm.manager.Manager
+
+import io.cloudevents.CloudEvent
+import io.cloudevents.core.v1.CloudEventBuilder
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+
+import io.cloudevents.CloudEventData
+import io.cloudevents.core.data.PojoCloudEventData
+import io.cloudevents.core.data.BytesCloudEventData
+
+import java.time.OffsetDateTime
+
+import java.net.URI
+import java.time.OffsetDateTime
+import java.util.UUID
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import com.fasterxml.jackson.databind.SerializationFeature
+import java.nio.charset.StandardCharsets
+
 
 import static org.freeplane.api.NodeChanged.ChangedElement
 
@@ -14,9 +38,8 @@ class TerminalNodeChangeListenerForModified implements NodeChangeListener {
         canReact = false
         switch (event.changedElement) {
             case [ChangedElement.TEXT, ChangedElement.NOTE, ChangedElement.ICON]:
-                def tmux_session_name = event.node.attributes.get("tmux session name")
-                def cmd = "bin/send-keys.sh " + tmux_session_name + " " + event.node.plainText
-                Runtime.runtime.exec(cmd)
+                def manager = new MindwmManager()
+                manager.sendCloudEvent('source', 'subject', 'type', '{"hello": "world"}')
         }
         canReact = true
     }
